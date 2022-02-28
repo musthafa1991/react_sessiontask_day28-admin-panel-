71106967bc310e088e5f4d0e432a8c883a358ef0
Mohamed Musthafa <musthafam39@gmail.com>
2022-02-28 12:52:00 +0530

    finished crud

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
 import logo from './logo.svg';
 import './App.css';
+import SideBAr from './sidebar';
+import TopBar from './topbar';
+import DashBoard from './dashboard';
+import UserList from './userlist';
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import CreateUser from './createuser';
+import UserView from './UserView';
+import UserEdit from './UserEdit';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <body id="page-top">
+        <div id="wrapper">
+          <SideBAr></SideBAr>
+
+          <div id="content-wrapper" className="d-flex flex-column">
+            <div id="content">
+              <TopBar></TopBar>
+              <div className="container-fluid">
+                <Routes>
+                  <Route path="/" element={<DashBoard />} />
+                  <Route path="/dashboard" element={<DashBoard />} />
+                  <Route path="/userlist" element={<UserList />} />
+                  <Route path="/createuser" element={<CreateUser/>} />
+                  <Route path="/UserView/:id" element={<UserView/>} />
+                  <Route path="/UserEdit/:id" element={<UserEdit/>} />
+                </Routes>
+
+              </div>
+
+
+            </div>
+          </div>
+
+
+
+
+        </div>
+      </body></BrowserRouter>
+
+
   );
 }
 
